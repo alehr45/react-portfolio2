6f6bc99d5bc990c747f0430de1769b84d2adc70b
Your NameAlex <alehr45@gmail.com>
2021-06-07 22:39:33 -0500

    homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Col , Image } from "react-bootstrap";
+import profile from "../images/Profile.jpg"
 
 
 function Home() {
     return (
+        <div>
+      <h1 class="title">-Alex Lehr-</h1>
       
-      <div>Hello World!</div>
-      
+    <div className="logo">
+      <Image className="pic" src={profile} height="260px" width="320px" roundedCircle/>
+      <h2 class="subtitle">-Full Stack Web Developer-</h2>
+      </div>
+    </div>
     );
   }
   

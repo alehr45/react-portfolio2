e53510b91f5f961c31ad4f952fe9d1738c1b717e
Your NameAlex <alehr45@gmail.com>
2021-06-07 22:50:56 -0500

    background color

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,8 @@ function Home() {
       <h1 class="title">-Alex Lehr-</h1>
       
     <div className="logo">
-      <Image className="pic" src={profile} height="260px" width="320px" roundedCircle/>
+    <style>{'body { background-color: antiquewhite; }'}</style>
+      <Image className="pic" src={profile} height="260px" width="310px" roundedCircle/>
       <h2 class="subtitle">-Full Stack Web Developer-</h2>
       </div>
     </div>

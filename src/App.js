37c9afe6830db2826d0d5d86de87343fa8634f1c
Your NameAlex <alehr45@gmail.com>
2021-06-16 21:41:38 -0500

    added portfolio pictures

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import AboutMe from "./components/AboutMe";
+import Portfolio from "./components/Portfolio";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
       <div className="App">
         <NavBar />
         <Switch>
+          <Route path="/portfolio" component={Portfolio} />
           <Route path="/aboutme" component={AboutMe} />
+
           <Route path="/" component={Home} />
         </Switch>
       </div>
---
+++ src/components/Portfolio.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Image, Container } from "react-bootstrap";
+
+import image0 from "../images/1.jpg";
+import image1 from "../images/2.jpg";
+import image2 from "../images/0.jpg";
+import image3 from "../images/3.jpg";
+import image4 from "../images/4.jpg";
+import image5 from "../images/5.jpg";
+
+function Portfolio() {
+  return (
+
+    
+    <Container className="portfolio1">
+      <style>{'body { background-color: antiquewhite; }'}</style>
+      <h1 className="work1">-Portfolio-</h1> 
+      <div>
+        <Image className="image1" height="600px" width="600px" src={image1} fluid />
+      </div>
+
+      <div>
+        <Image  className="image1" height="600px" width="600px" src={image2} fluid />
+      </div>
+
+      <div>
+        <Image  className="image1" height="600px" width="600px" src={image0} fluid />
+      </div>
+
+      <div>
+        <Image  className="image1" height="600px" width="600px" src={image4} fluid />
+      </div>
+
+      <div>
+        <Image className="image1" height="600px" width="600px" src={image5} fluid />
+      </div>
+    </Container>
+  );
+}
+
+export default Portfolio;

4bb368a4711d51e07e189962b078fb60ac3e4b66
Your NameAlex <alehr45@gmail.com>
2021-06-18 12:56:03 -0500

    added links to images in portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Container } from "react-bootstrap";
+import { Image, Container, Button } from "react-bootstrap";
 import TextAnimation from "react-animate-text";
 import { SocialIcon } from "react-social-icons";
 import image0 from "../images/1.jpg";
@@ -14,6 +14,7 @@ function Portfolio() {
       <style>{"body { background-color: antiquewhite; }"}</style>
       <TextAnimation>
         <h1 className="work1">-Portfolio-</h1>
+        </TextAnimation>
 <SocialIcon
           className="social"
           url="https://www.linkedin.com/in/alexlehr/"
@@ -23,13 +24,14 @@ function Portfolio() {
           url="https://www.facebook.com/alex.lehr.75"
         />
         <SocialIcon className="social" url="https://github.com/alehr45" />
-      </TextAnimation>
-      <div className="border">
-        <h5>Haul That</h5>
+      
+      <div>
+        <div>
+        <h5 class="title1">Haul That</h5>
         <h6 className="description">A P2P local frieght delivery service.</h6>
-        <a href="https://haul-that.herokuapp.com/">
-          https://haul-that.herokuapp.com/
-        </a>
+        
+        </div>
+        <a href="https://haul-that.herokuapp.com/ " target="_blank">
         <Image
           className="image1"
           height="600px"
@@ -37,23 +39,16 @@ function Portfolio() {
           src={image1}
           fluid
         />
+        </a>
+        
       </div>
-
-      <div className="border">
-        <h5>Weekly Food Planner</h5>
-        <h6 className="description">A meal prep application using Spoonacular API.</h6>
-        <Image
-          className="image1"
-          height="600px"
-          width="600px"
-          src={image2}
-          fluid
-        />
-      </div>
-
-      <div className="border">
-        <h5>GamerSpot</h5>
+     
+<div>
+<div >
+        <h5 class="title1">GamerSpot</h5>
         <h6 className="description">A video game e-commerce site using Handlebars.</h6>
+        </div>
+        <a href="https://gamer-spot-45.herokuapp.com/" target="_blank">
         <Image
           className="image1"
           height="600px"
@@ -61,13 +56,33 @@ function Portfolio() {
           src={image0}
           fluid
         />
+        </a>
       </div>
-
-      <div className="border">
-        <h5>Run Buddy</h5>
+      
+<div>
+<div >
+        <h5 class="title1">Weekly Food Planner</h5>
+        <h6 className="description">A meal prep application using Spoonacular API.</h6>
+        </div>
+        <a href="https://haul-that.herokuapp.com/ " target="_blank">
+        <Image
+          className="image1"
+          height="600px"
+          width="600px"
+          src={image2}
+          fluid
+        />
+        </a>
+      </div>
+      
+<div>
+      <div >
+        <h5 class="title1">Run Buddy</h5>
         <h6 className="description">
           A fitness website that helps you keep in touch with your trainers.{" "}
         </h6>
+        </div>
+        <a href="https://haul-that.herokuapp.com/" target="_blank">
         <Image
           className="image1"
           height="600px"
@@ -75,13 +90,16 @@ function Portfolio() {
           src={image5}
           fluid
         />
+        </a>
       </div>
-
-      <div className="border">
-        <h5>Budget Tracker</h5>
+<div>
+      <div >
+        <h5 class="title1">Budget Tracker</h5>
         <h6 className="description">
           An app that helps you keep track of your budget, online or off.{" "}
         </h6>
+        </div>
+        <a href="https://haul-that.herokuapp.com/" target="_blank">
         <Image
           className="image1"
           height="600px"
@@ -89,10 +107,12 @@ function Portfolio() {
           src={image4}
           fluid
         />
+        </a>
       </div>
       <div>
         <h1 className="copy1">-Alex Lehr &copy; 2021-</h1>
       </div>
+     
     </Container>
   );
 }
